a20b223ec3d9c5bfd340653ef191ffdbd1f0c098
 <sammy.whythim@gmail.com>
2017-02-10 17:33:24 +0000

    Added thread control and looping through the list

--- main.py
+++ main.py
@@ -1,35 +1,112 @@
-from threading import Thread
+from msvcrt import getch
+import threading
 import random
 import time
+import sys
 
 
-def one_thread(poem):
-    for x in range(0, len(poem)):
-        pos = x
-        time.sleep(.3)
-        print poem[x]
+lock = threading.Lock()
 
-def anotha_thread(poem):
+def thread(poem):
     global pos
-    for y in range(0, len(poem)):
-        pos = y
+    
+    while (pos < count):
+        lock.acquire()
+        pause()
+        pos += 1
+        pos = pos_reset(pos)
+        
         time.sleep(.2)
-        print poem[y]
+        lock.release()    
 
+def thread2(poem):
+    global pos
+    while (pos < count):
+        lock.acquire()
+        pause()
+        if (pos < count):
+            print poem[pos]
+
+        time.sleep(.2)
+        lock.release()
+
+def thread3(poem):
+    global key, ay
+    luck = threading.Lock()
+    while keysearch == True:
+        with luck:
+            key = getch()
+            if (ay==True):
+                ay = False
+    
+def pos_reset(n):
+    if (n == count):
+        return 0
+    else:
+        return n
+
+def pause():
+    global key, ay, pos, keysearch
+    
+    cv.acquire()
+    if (key=="1"):
+        print "\nPAUSING THREADS..."
+        secs = random.randint(1, 4)
+        time.sleep(secs)
+        ay = True
+
+        informed=False
+        while ay == True:
+            cv.wait(.1)
+            if informed == False:
+                print "\nPAUSED\n\nPRESS ANY KEY TO RESUME...\n"
+                informed = True
+            
+        cv.notifyAll()
+        cv.release()
+        key =4
+        
+    elif (key=="2"):
+        print '---PRESS ANY KEY TO CLOSE PROGRAM---'
+        pos = count
+        keysearch = False
+        time.sleep(.5)
+        cv.notifyAll()
+        cv.release()
+        key = 4
+        
+    else:
+        cv.notifyAll()
+        cv.release()
 
 def main():
-    global pos
-    t = None
+    global pos, count, key, cv, ay, keysearch
+    
+    key = "lol"
+    play = True
+    pos = -1
+    keysearch =True
 
     with open('caged_bird.txt') as f:
         data = f.readlines()
+    
+    count = len(data)
 
-    t1 = Thread(target=one_thread, args=(data,))
-    t2 = Thread(target=anotha_thread, args=(data,))
+    cv = threading.Condition()
+    ay = False
+    t1 = threading.Thread(target=thread, args=(data,))
+    t2 = threading.Thread(target=thread2, args=(data,))
+    t3 = threading.Thread(target=thread3, args = (data,))
+
     t1.start()
     t2.start()
+    t3.start()
+ 
+    
     t1.join()
     t2.join()
+    t3.join()
+    
 
 if __name__ == '__main__':
     main()
